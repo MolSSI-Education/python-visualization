310cbd9956119e00414bfc2a35e92a11ded7173e
Jessica Nash <janash@vt.edu>
2021-02-14 15:22:27 -0500

    add script for google analytics

---
+++ molssi_python_visualization/_static/add_analytics.js
@@ -0,0 +1,10 @@
+
+document.head.innerHTML += `<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-169902961-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-169902961-1');
+</script>"`
